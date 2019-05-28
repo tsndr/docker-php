bc8ad1eeb5099fb465921e3021a68ea6e0d8102e
Tobias Schneider <mail@tobias-schneider.io>
2019-05-28 23:59:24 +0200

    Updated helper scripts

--- bash.sh
+++ bash.sh
@@ -4,4 +4,5 @@ docker run \
     -it \
     -p 80:80 \
     -v "$(pwd)"/html:/var/www \
+    -v "$(pwd)"/db:/var/lib/mysql \
     tsndr/php:7.3 bash
--- run.sh
+++ run.sh
@@ -4,4 +4,5 @@ docker run \
     -d \
     -p 80:80 \
     -v "$(pwd)"/html:/var/www \
+    -v "$(pwd)"/db:/var/lib/mysql \
     tsndr/php:7.3
