7b375a1e47f07cc9d5bf005b53e4d5ad6194cc4e
Tobias Schneider <mail@tobias-schneider.io>
2019-05-28 23:55:08 +0200

    Updated minify script

--- config/minify.sh
+++ config/minify.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
-export="min"
-mkdir -p $export
+src="src"
+dist="dist"
 
-for f in *.conf
+for f in $(find $src/ -type f)
 do
+    p=$(dirname -- $f)
+    f=$(basename -- $f)
+    o="${p/$src/$dist}"
+    if [ ! -d $o ]
+    then
+        mkdir -p $o
+    fi
     printf "Processing $f ..."
-    sed 's/\$/\\$/g' $f > $export/$f
-    sed -i 's/    /\\t/g' $export/$f
-    sed -i 's/\t/\\t/g' $export/$f
-    sed -i 's/\"/\\"/g' $export/$f
-    sed -i ':a;N;$!ba;s/\n/\\n/g' $export/$f
-    echo "echo \"$(cat $export/$f)\"" > $export/$f
+    sed 's/\$/\\$/g' $p/$f > $o/$f
+    sed -i 's/    /\\t/g' $o/$f
+    sed -i 's/\t/\\t/g' $o/$f
+    sed -i 's/\"/\\"/g' $o/$f
+    sed -i ':a;N;$!ba;s/\n/\\n/g' $o/$f
+    echo "echo \"$(cat $o/$f)\"" > $o/$f
     printf " ok\n"
 done
